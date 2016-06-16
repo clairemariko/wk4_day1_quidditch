fb35feb5fdc53a3b166d75b162832a9c4991acae
claire <claire.mariko.smith@gmail.com>
2016-06-16 17:47:00 +0100

    update

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require( 'pry-byebug' )
 require_relative('controllers/teams_controller')
 require_relative('controllers/players_controller')
+require_relative()
 require 'sinatra/contrib/all' if development?
 
 get '/' do
